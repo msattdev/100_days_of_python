4a9000bc6caa45aa4acb84dda2db1cfeb830c703
Matt Satterfield <mattsattdev@gmail.com>
2025-12-29 23:18:57 -0500

    band name generator

---
+++ Day1-BandNameGenerator/bandname.py
@@ -0,0 +1,9 @@
+# Band Name Generator
+# Welcome message
+print("Welcome to the Band Name Generator!")
+# Ask user for city name saved in variable
+city = input("What's the name off the city you grew up in?\n")
+# Ask user for pet name saved in variable
+pet = input("What's your pet's name?\n")
+# Combine city and pet name to generate the possible band name
+print(f"Your band name could be {city} {pet}")
