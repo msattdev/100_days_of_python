23765d7f1bbbcc376f8665a1135d8c100259b532
msattdev <mattsattdev@gmail.com>
2026-01-01 18:59:46 -0500

    day6

---
+++ Day6-EscapeTheMaze/defining_functions.py
@@ -0,0 +1,6 @@
+def this_is_my_function():
+    print("This is me, defining a function in Python!")
+    print("This is a very useful skill to have.")
+    print("Functions help to organize code into reusable blocks.")
+
+this_is_my_function()
