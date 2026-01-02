4bd43ae55582fbf72184c6ec36c7cc0420ff67c2
msattdev <mattsattdev@gmail.com>
2026-01-02 16:54:04 -0500

    updates to hangman

--- Day7-Hangman/hangman.py
+++ Day7-Hangman/hangman.py
@@ -7,17 +7,33 @@ print("Welcome to Hangman!")
 # Radnomly choose a word from a predefined list
 random_words = ["python", "java", "kotlin", "javascript","banana"]
 chosen_word = random.choice(random_words)
+placeholder = "_" * len(chosen_word)
+print(placeholder)
 print(chosen_word)  # For testing purposes; remove in production
 
-# User guesses a letter
-user_guess = input("Guess a letter: ").lower()
+game_over = False
+correct_letters = []
 
-# Check if the user guessed letter is in the randomly chosen word
-# Iterate through each letter in the chosen word
-for letter in chosen_word:
-    if letter == user_guess:
-        print("Correct!")
-    else:
-        print("Wrong! Guess again!")
+while not game_over:
+    # User guesses a letter
+    user_guess = input("Guess a letter: ").lower()
+    # Empty string to store the guessed letters
+    display = ""
+    # Check if the user guessed letter is in the randomly chosen word
+    # Iterate through each letter in the chosen word
+    for letter in chosen_word:
+        if letter == user_guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
 
+    print(display)
+
+    # Check if the user has guessed all letters in the word
+    if "_" not in display:
+        game_over = True
+        print("You win!")
 
