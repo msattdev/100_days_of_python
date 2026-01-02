ed7baa7b0d6f39893a7228e4d8492d5d712fbe32
msattdev <mattsattdev@gmail.com>
2026-01-02 15:42:14 -0500

    updates

--- Day7-Hangman/hangman.py
+++ Day7-Hangman/hangman.py
@@ -1,8 +1,23 @@
-# hangman 
+# Hangman Game
 import random
 
+# Display a welcome message for the user
 print("Welcome to Hangman!")
 
-random_words = ["python", "java", "kotlin", "javascript"]
-
+# Radnomly choose a word from a predefined list
+random_words = ["python", "java", "kotlin", "javascript","banana"]
 chosen_word = random.choice(random_words)
+print(chosen_word)  # For testing purposes; remove in production
+
+# User guesses a letter
+user_guess = input("Guess a letter: ").lower()
+
+# Check if the user guessed letter is in the randomly chosen word
+# Iterate through each letter in the chosen word
+for letter in chosen_word:
+    if letter == user_guess:
+        print("Correct!")
+    else:
+        print("Wrong! Guess again!")
+
+
