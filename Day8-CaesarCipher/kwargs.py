fcedad0c58a9c659a7592fe58f70bf8ce96f14f4
msattdev <mattsattdev@gmail.com>
2026-01-03 18:19:53 -0500

    kwargs py

---
+++ Day8-CaesarCipher/kwargs.py
@@ -0,0 +1,8 @@
+# Functions with more than one input
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"What is it like in {location}?")
+
+greet_with("Alice", "Wonderland")
+
+greet_with(name="Bob", location="Builderland")
