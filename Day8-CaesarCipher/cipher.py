92d5edaf51935f47eb9c0d51eb8404713882f476
msattdev <mattsattdev@gmail.com>
2026-01-03 18:54:41 -0500

    added decrypt function

--- Day8-CaesarCipher/cipher.py
+++ Day8-CaesarCipher/cipher.py
@@ -13,4 +13,12 @@ def encrypt(original_text, shift_amount):
         cipher_text += alphabet[shifted_position]
     print(f"The encoded text is: {cipher_text}")
 
-encrypt(original_text=text, shift_amount=shift)
+def decrypt(original_text, shift_amount):
+    decoded_text = ""
+    for letter in original_text:
+        original_position = alphabet.index(letter) - shift_amount
+        original_position %= len(alphabet)
+        decoded_text += alphabet[original_position]
+    print(f"The decoded text is: {decoded_text}")
+
+decrypt(original_text=text, shift_amount=shift)
