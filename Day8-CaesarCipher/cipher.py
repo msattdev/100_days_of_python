3738c5be0e744464b900e4340cecf77621c92b5b
msattdev <mattsattdev@gmail.com>
2026-01-03 19:09:24 -0500

    commented out encrypt and decrypt

--- Day8-CaesarCipher/cipher.py
+++ Day8-CaesarCipher/cipher.py
@@ -5,21 +5,21 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text, shift_amount):
-    cipher_text = ""
-    for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"The encoded text is: {cipher_text}")
+# def encrypt(original_text, shift_amount):
+#     cipher_text = ""
+#     for letter in original_text:
+#         shifted_position = alphabet.index(letter) + shift_amount
+#         shifted_position %= len(alphabet)
+#         cipher_text += alphabet[shifted_position]
+#     print(f"The encoded text is: {cipher_text}")
 
-def decrypt(original_text, shift_amount):
-    decoded_text = ""
-    for letter in original_text:
-        original_position = alphabet.index(letter) - shift_amount
-        original_position %= len(alphabet)
-        decoded_text += alphabet[original_position]
-    print(f"The decoded text is: {decoded_text}")
+# def decrypt(original_text, shift_amount):
+#     decoded_text = ""
+#     for letter in original_text:
+#         original_position = alphabet.index(letter) - shift_amount
+#         original_position %= len(alphabet)
+#         decoded_text += alphabet[original_position]
+#     print(f"The decoded text is: {decoded_text}")
 
 # decrypt(original_text=text, shift_amount=shift)
 
