6ad9f984a859c56e218f73bc91910f18f47e55c2
msattdev <mattsattdev@gmail.com>
2026-01-04 18:12:45 -0500

    updates to optimize further

--- Day9-Auction/auction_optimized.py
+++ Day9-Auction/auction_optimized.py
@@ -2,19 +2,23 @@ from art import logo
 import os
 print(logo)
 
+# Defining a function to find the highest bidder from the bids_dict dictionary
 def find_highest_bidder(bids_dict):
-    for bids in bids_dict:
-        highest_bidder = 0
-        winner = ""
-        bid_amount = bids_dict[bids]
-        if bid_amount > highest_bidder:
-            highest_bidder = bid_amount
-            winner = bids
+    for bids in bids_dict: # for loop to iterate through each bid in the dictionary
+        highest_bidder = 0 # variable to keep track of the highest bid found; initalized to 0
+        winner = "" # variable to store the name of the highest bidder; initialized to an empty string
+        bid_amount = bids_dict[bids] # retrieves the bid amount for the current bidder
+        if bid_amount > highest_bidder: # compares the current bid amount with the highest bid found so far
+            highest_bidder = bid_amount # updates the highest bid if the current bid is greater
+            winner = bids # updates the winner to the current bidder's name
+    os.system('clear') # clears the console for better readability
     print(f"The winner is {winner} with a bid of ${highest_bidder}!")
-        
+
+# Empty dictonary established to store the bids     
 bids_dict = {}
 should_continue = True
 
+# This is the main bidding loop - if the user types 'yes' for more bidders, then the program will continue to run until 'no' is typed   
 while should_continue:
     name = input("What is your name?: ")
     bid_amount = int(input("What is your bid amount?: $"))
