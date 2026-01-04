72fbc9f76cdc3b317218a34b920ef34b4f233ab2
msattdev <mattsattdev@gmail.com>
2026-01-03 19:34:50 -0500

    final

--- Day8-CaesarCipher/cipher.py
+++ Day8-CaesarCipher/cipher.py
@@ -16,9 +16,34 @@ print("============================================")
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    for letter in original_text:
+        if letter in alphabet:
+            if encode_or_decode == "encode":
+                shifted_position = alphabet.index(letter) + shift_amount
+            elif encode_or_decode == "decode":
+                shifted_position = alphabet.index(letter) - shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += letter
+    print(f"The {encode_or_decode}d text is: {output_text}", flush=True)
+
+should_continue = True
+
+while should_continue:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye!")
 
 # def encrypt(original_text, shift_amount):
 #     cipher_text = ""
@@ -38,18 +63,5 @@ shift = int(input("Type the shift number:\n"))
 
 # decrypt(original_text=text, shift_amount=shift)
 
-def caesar(original_text, shift_amount, encode_or_decode):
-    output_text = ""
-    for letter in original_text:
-        if letter in alphabet:
-            if encode_or_decode == "encode":
-                shifted_position = alphabet.index(letter) + shift_amount
-            elif encode_or_decode == "decode":
-                shifted_position = alphabet.index(letter) - shift_amount
-            shifted_position %= len(alphabet)
-            output_text += alphabet[shifted_position]
-        else:
-            output_text += letter
-    print(f"The {encode_or_decode}d text is: {output_text}")
 
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
